ac93bf5bbf484ed406fac9432503ed7f36ef4219
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2019-01-28 20:51:15 -0200

    moinhos + nuvens + background

---
+++ Assets/Scripts/Scroll_UI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll_UI : MonoBehaviour {
+
+    public RawImage back;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        back.uvRect = new Rect(0.03f * Time.time, 0, 1, 1);
+
+    }
+}
